20ef94e28bd2d6207b6ca981f23423556fada745
Nguyễn Tuấn Cường <ntuancuong2005@gmail.com>
2025-11-09 11:29:49 +0700

    Tạo thư mục views

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
+//Thiết lập thư mục chứa pug
+// app.set("views", "./views");
+app.set("views", path.join(__dirname, "views"));
+
+//Thiết lập pug làm view engine
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  res.send("Hello World123!");
+  res.render("client/pages/home", {
+    pageTitle: "Trang chủ",
+  });
+});
+
+app.get("/tours", (req, res) => {
+  res.render("client/pages/tour-list", {
+    pageTitle: "Danh sách tours",
+  });
 });
 
 app.listen(port, () => {
